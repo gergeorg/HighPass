686f190be05db7f9bdd0dd831ff0bb8ad405a182
gergeorg <georgey.gerasimov@gmail.com>
2022-04-02 03:47:32 +0300

    add full decktop version

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -1,32 +1,19 @@
 ymaps.ready(init);
 function init() {
-  var myMap = new ymaps.Map("map", {
-    center: [55.76953456898229, 37.63998549999998],
+  const myMap = new ymaps.Map("map", {
+    center: [55.76892370723494, 37.63811868252562],
     zoom: 17
   });
 
-  var myPlacemark = new ymaps.Placemark([55.76953456898229, 37.63998549999998], {
-
-    balloonContent: `
-    <div class="balloon1">
-      <h3 class="balloon__title">Студия “High pass”</h3>
-
-      <address class="balloon__address">107045, Москва, Даев переулок, дом 41, бизнес-центр «Даев Плаза», этаж 8, офис №
-        82 </address>
-      <a href="tel:+749542423532" class="balloon__tel">+7 (495) 42-423-532</a>
-    </div>
-    `
-
-  }, {
+  const myPlacemark = new ymaps.Placemark([55.76953456898229, 37.63998549999998], {}, {
     iconLayout: 'default#image',
     iconImageHref: 'img/placemark.svg',
-    iconImageSize: [28, 40],
+    iconImageSize: [12, 12],
     iconImageOffset: [-3, -42],
   });
 
   myMap.geoObjects.add(myPlacemark);
 
-  myPlacemark.balloon.open()
 
   myMap.controls.remove('geolocationControl'); // удаляем геолокацию
   myMap.controls.remove('searchControl'); // удаляем поиск
@@ -36,5 +23,14 @@ function init() {
   myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
   myMap.controls.remove('rulerControl'); // удаляем контрол правил
 
+  const contactsInfo = document.querySelector('.contacts__info')
+  const contactsBtnClose = document.querySelector('.contacts__btn-close')
 
+  contactsBtnClose.addEventListener('click', () => {
+    contactsInfo.classList.add('contacts__info--close')
+  })
+
+  myPlacemark.addEventListener('click', () => {
+    contactsInfo.classList.add('contacts__info--close')
+  })
 }
